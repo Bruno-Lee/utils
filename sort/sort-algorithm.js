13c74eca0fc711b4ffaf51287a6ea0595fa55d94
Bruno-Lee <lizhao@Lizhao.kingdee.gbl>
2015-04-25 11:23:34 +0800

    react-learn

--- sort/sort-algorithm.js
+++ sort/sort-algorithm.js
@@ -37,27 +37,29 @@ var sort = {
                 i = l,
                 j = r;
 
-            while (i < j) {
+            while (i < j) {  //从右向左比较，如果该元素大于哨兵则继续向左移，直到遇到小于哨兵的元素
                 while ((desc === true ? d[j] < base : d[j] > base) && i < j) {
                     j--;
                 }
 
-                if (i < j) {
+                if (i < j) {  //将小于哨兵的元素交换到左侧部分
                     d[i] = d[j];
                     i++;
                 }
 
                 while ((desc === true ? d[i] > base : d[i] < base) && i < j) {
+                    //从左向右比较，如果该元素小于哨兵则继续向右移，直到遇到大于哨兵的元素
                     i++;
                 }
 
-                if (i < j) {
+                if (i < j) {  //将小于哨兵的元素交换到右侧部分
                     d[j] = d[i];
                     j--;
                 }
             }
 
             d[i] = base;
+            //分别将比哨兵小的集合和比哨兵大的集合再次快排
             arguments.callee(d, desc, l, i - 1);
             arguments.callee(d, desc, j + 1, r);
         }
